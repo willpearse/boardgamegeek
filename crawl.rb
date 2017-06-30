12b14b3c817a882c0fbe564fdec0560c20c0c66c
Will Pearse <will.pearse@gmail.com>
2017-06-30 09:57:50 -0600

    More detailed data

--- crawl.rb
+++ crawl.rb
@@ -26,7 +26,7 @@ end.parse!
 got_games = 0; curr_page = 1; rank = 1
 
 CSV.open(options[:output], "w") do |row|
-  row << ["rank", "ID", "name", "mechanics", "families", "categories", "n.ratings", "rating"]
+  row << ["rank", "ID", "name", "mechanics", "families", "categories", "n.ratings", "rating", "bayes.rating", "owned", "one.player","two.players","three.players","four.players","many.players", "age", "language", "n.weight","weight"]
 
   while got_games < options[:number]
     page = Nokogiri::HTML(open("https://boardgamegeek.com/browse/boardgame/page/#{curr_page}").read)
@@ -41,10 +41,42 @@ CSV.open(options[:output], "w") do |row|
       mechanics = game.css("boardgamemechanic").map {|x| x.children.text}.join "|"
       families = game.css("boardgamefamily").map {|x| x.children.text}.join "|"
       categories = game.css("boardgamecategory").map {|x| x.children.text}.join "|"
+
+      
       n_ratings = game.css("usersrated").text.to_i
       rating = game.css("average").text.to_f
+      bayes_rating = game.css("bayesaverage").text.to_f
+      owned = game.css("owned").text.to_i
 
-      row << [rank, id, name, mechanics, families, categories, n_ratings, rating]
+      one_player = [1,3,5].map {|x| game.css("results[numplayers='1']")[0].children[x].attributes["numvotes"].text.to_i}.join "|"
+      begin
+        two_players = [1,3,5].map {|x| game.css("results[numplayers='2']")[0].children[x].attributes["numvotes"].text.to_i}.join "|"
+      rescue
+        two_players = "NA"
+      end
+      begin
+        three_players = [1,3,5].map {|x| game.css("results[numplayers='3']")[0].children[x].attributes["numvotes"].text.to_i}.join "|"
+      rescue
+        three_players = "NA"
+      end
+      begin
+        four_players = [1,3,5].map {|x| game.css("results[numplayers='4']")[0].children[x].attributes["numvotes"].text.to_i}.join "|"
+      rescue
+        four_players = "NA"
+      end
+      begin
+        many_players = [1,3,5].map {|x| game.css("results[numplayers='4+']")[0].children[x].attributes["numvotes"].text.to_i}.join "|"
+      rescue
+        many_players = "NA"
+      end
+      age = [1,3,5,7,9,11,13,15,17].map {|x| game.css("poll[name='suggested_playerage']").children[1].children[x]["numvotes"].to_i}.join "|"
+      language = [1,3,5,7,9].map {|x| game.css("poll[name='language_dependence']").children[1].children[x]["numvotes"].to_i}.join "|"
+
+      weight = game.css("averageweight").text.to_i
+      n_weight = game.css("numweight").text.to_i
+
+      
+      row << [rank, id, name, mechanics, families, categories, n_ratings, rating, bayes_rating, owned, one_player,two_players,three_players,four_players,many_players, age, language, n_weight, weight]
       
       got_games += 1
       rank += 1
